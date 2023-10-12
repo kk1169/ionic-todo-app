1adec171d96f782f34b7b6b13d6b28e4884c6317
kk1169 <39980501+kk1169@users.noreply.github.com>
2023-10-13 00:13:30 +0530

    Remove api folder

--- api/src/router/authentication.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-import { register, login } from '../controllers/authentication';
-
-export default (router: express.Router) => {
-  router.post('/auth/login', login);
-  router.post('/auth/register', register);
-};
--- api/src/router/users.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-import { getAllUsers } from '../controllers/users';
-import { isAuthenticated } from '../middlewares';
-
-export default (router: express.Router) => {
-  router.get('/users', isAuthenticated, getAllUsers);
-};
